562c8b79fb20b8b1d5b17e57090adc6c7da09a2a
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2020-11-23 23:31:57 -0600

    Update Tip_Calculator.py

--- D2_Tip_Calculator/Tip_Calculator.py
+++ D2_Tip_Calculator/Tip_Calculator.py
@@ -4,9 +4,9 @@ bill = float(input("What was the total bill? $"))
 percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 people_count = int(input("How many people to split the bill? "))
 
-tip_total = bill * (percentage / 100)
-total_bill = bill + tip_total
-individual_tip = total_bill / people_count
-final_amount = round(individual_tip, 2)
+tip = bill * (percentage / 100)
+total_bill = bill + tip
+individual_amount = total_bill / people_count
+final_amount = round(individual_amount, 2)
 
 print(f"Each person should pay ${final_amount}")
