52265c9fe026c91362cd9c99f4e20cacac3aa399
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2020-12-23 10:36:02 -0600

    Add files via upload

---
+++ D3.1 Rollercoaster/rollercoaster.py
@@ -0,0 +1,34 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+total_bill = 0
+
+def photograph():
+    question = str(input("Want photos? ")).upper()
+    if question == "YES":
+        print("Photographs are $3.")
+        global total_bill
+        total_bill += 3
+        print(f"The total_bill is {total_bill}")
+    elif question == "NO":
+        print(f"The total_bill is {total_bill}")
+    else: 
+        print("Please respond with Yes or No")
+        photograph()
+
+if height > 120:
+    print("You can ride the rollercoaster.")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Child tickets are $5.")
+        total_bill += 5
+        photograph()
+    elif age <= 18:
+        print("Youth tickets are $7.")
+        total_bill += 7
+        photograph()
+    else:
+        print("Adult tickets are $12.")
+        total_bill += 12
+        photograph()
+else: 
+    print("Sorry you have to grow taller.")
