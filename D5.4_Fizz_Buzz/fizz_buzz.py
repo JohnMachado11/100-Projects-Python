c8c8d1ebb5d3d456a6953a2ed29b76c88add703a
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2021-02-15 22:13:35 -0600

    Update fizz_buzz.py

--- D5.4_Fizz_Buzz/fizz_buzz.py
+++ D5.4_Fizz_Buzz/fizz_buzz.py
@@ -1,6 +1,6 @@
 
 for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0: # or i % 15
+    if i % 3 == 0 and i % 5 == 0: # Or "i % 15 == 0"
         print("FizzBuzz")
     elif i % 3 == 0:
         print("Fizz")
