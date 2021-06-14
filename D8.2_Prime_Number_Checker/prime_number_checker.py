a2ade7507abfdbac4e95224f43533fec1111b826
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2021-06-14 11:30:50 -0500

    Update prime_number_checker.py

--- D8.2_Prime_Number_Checker/prime_number_checker.py
+++ D8.2_Prime_Number_Checker/prime_number_checker.py
@@ -1,3 +1,6 @@
+# "A natural number which has exactly two factors, i.e. 1 and the number itself, is a prime number. 
+# In simple words, if a number is only divisible by 1 and itself, then it is a prime number. 
+# Every prime number is an odd number except number 2."
 
 def prime_checker(number):
     is_prime = True
@@ -13,6 +16,3 @@ def prime_checker(number):
 
 n = int(input("Check this number: "))
 prime_checker(number=n)
-
-    # if number == 2:
-    #     print("It's a prime number.")
