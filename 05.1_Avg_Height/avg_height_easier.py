f87f80e60636f62c6fdaa66ac2949f651d20146a
John Machado <58408039+JohnMachado11@users.noreply.github.com>
2021-07-15 12:28:53 -0500

    Add files via upload

---
+++ 05.1_Avg_Height/avg_height_easier.py
@@ -0,0 +1,13 @@
+# AVERAGE HEIGHT OF ALL STUDENTS
+
+# Example input = 67, 82, 51, 31, 21
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+
+# Method 1 - Easy
+total_height = sum(student_heights)
+number_of_students = len(student_heights)
+average_height = round(total_height / number_of_students)
+
+print(f"Students average height = {average_height}.")
---
+++ 05.1_Avg_Height/avg_height_harder.py
@@ -0,0 +1,17 @@
+# AVERAGE HEIGHT OF ALL STUDENTS
+
+# Example input = 67, 82, 51, 31, 21
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+
+# Method 2 - Harder
+num_students = 0
+total_height = 0 
+
+for i in range(0, len(student_heights)):
+    total_height += student_heights[i]
+    num_students += 1
+
+avg = round(total_height / num_students)
+print(f"Students average height = {avg}.")
